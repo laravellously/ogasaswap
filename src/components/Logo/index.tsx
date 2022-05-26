f0d4132fb11d0b55b058a4450aae750f04d8ff5c
Martins 'Laravellous Michael <martins.michael71@gmail.com>
2022-05-26 11:11:18 +0100

    Update index.tsx

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -21,72 +21,10 @@ const LogoSignWrapper = styled(Box)(
 `
 );
 
-const LogoSign = styled(Box)(
-  ({ theme }) => `
-        background: ${theme.general.reactFrameworkColor};
-        width: 18px;
-        height: 18px;
-        border-radius: ${theme.general.borderRadiusSm};
-        position: relative;
-        transform: rotate(45deg);
-        top: 3px;
-        left: 17px;
-
-        &:after, 
-        &:before {
-            content: "";
-            display: block;
-            width: 18px;
-            height: 18px;
-            position: absolute;
-            top: -1px;
-            right: -20px;
-            transform: rotate(0deg);
-            border-radius: ${theme.general.borderRadiusSm};
-        }
-
-        &:before {
-            background: ${theme.palette.primary.main};
-            right: auto;
-            left: 0;
-            top: 20px;
-        }
-
-        &:after {
-            background: ${theme.palette.secondary.main};
-        }
-`
-);
-
-const LogoSignInner = styled(Box)(
-  ({ theme }) => `
-        width: 16px;
-        height: 16px;
-        position: absolute;
-        top: 12px;
-        left: 12px;
-        z-index: 5;
-        border-radius: ${theme.general.borderRadiusSm};
-        background: ${theme.header.background};
-`
-);
-
 const LogoTextWrapper = styled(Box)(
   ({ theme }) => `
         padding-left: ${theme.spacing(1)};
-`
-);
-
-const VersionBadge = styled(Box)(
-  ({ theme }) => `
-        background: ${theme.palette.success.main};
-        color: ${theme.palette.success.contrastText};
-        padding: ${theme.spacing(0.4, 1)};
-        border-radius: ${theme.general.borderRadiusSm};
-        text-align: center;
-        display: inline-block;
-        line-height: 1;
-        font-size: ${theme.typography.pxToRem(11)};
+        margin-top: ${theme.spacing(2)};
 `
 );
 
@@ -98,12 +36,10 @@ const LogoText = styled(Box)(
 );
 
 function Logo() {
-
-
   return (
     <LogoWrapper to="/">
       <LogoSignWrapper>
-        <Avatar src="/ogasa.png"></Avatar>
+        <Avatar src="/ogasa.png" sx={{ width: 56, height: 56 }}></Avatar>
       </LogoSignWrapper>
       <Hidden smDown>
         <LogoTextWrapper>
